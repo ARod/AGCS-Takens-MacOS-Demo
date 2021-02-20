a23f1107cc23de1c53a9a8f024edf6a7f9ae5c4a
Dr. Alberto Rodriguez (AJ Technologies LLC) <DrARod@192.168.1.13>
2021-02-20 12:13:54 -0600

    Initial Commit

---
+++ Talos Demo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Talos Demo
+//
+//  Created by Dr. Alberto Rodriguez on 2/20/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Talos Demo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Talos Demo
+//
+//  Created by Dr. Alberto Rodriguez on 2/20/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
